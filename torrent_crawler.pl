6b882352215a4f300df6e42e10e2c849f0b1c918
Alex <alejandro.amorin@gmail.com>
2017-08-08 17:44:32 +0200

    Download from all sites in one search and code refactoring

--- torrent_crawler.pl
+++ torrent_crawler.pl
@@ -16,6 +16,8 @@ my $web_call;
 my $selected_url;
 my $downloads_size;
 my $search_word;
+my @urls_all;
+my $initial_se;
 
 my $HYPHEN 	= '-';
 my $PLUS	= '+';
@@ -42,6 +44,8 @@ if ($ARGV[0]) {
 	$search_word = $keyword;
 }
 
+$search_word =~ tr/"'//ds;
+
 my $size = Get2DArraySize(@WEBS);
 
 print "\n";
@@ -53,12 +57,28 @@ print "\n";
 if ($se eq "C" || $se eq "c") {
 	die "\nQuitting." 
 } else {
-	$se = $se - 1;
-	$BASE_URL 	= $WEBS[$se][0];
-	$SEARCH_URL = $BASE_URL.$WEBS[$se][1];
+	if($se eq "0") {
+		$initial_se = $se;
+		my $download_content;
+		for (my $i=0; $i < $size; $i++) {
+			$se = $i;
+			$BASE_URL 	= $WEBS[$se][0];
+			$SEARCH_URL = $BASE_URL.$WEBS[$se][1];
+			my $link_content = GetLinks();
+			my @urls = GetDownloadsURL( $link_content, 0 );
+			push @urls_all, @urls;
+		}
+		GetDownloadsTorrent( 0, @urls_all );
+	} else {
+		$initial_se = $se;
+		$se = $se - 1;
+		$BASE_URL 	= $WEBS[$se][0];
+		$SEARCH_URL = $BASE_URL.$WEBS[$se][1];
+		my $link_content = GetLinks();
+		my @urls = GetDownloadsURL( $link_content, 0 );
+		GetDownloadsTorrent( 0, @urls );
+	}
 }
-
-my @links = GetLinks();
 
 #
 # Intro menu
@@ -66,6 +86,7 @@ my @links = GetLinks();
 sub AskPage {
 	print "\n\t\tIn which page do you want to search?: ";
 	
+	print "\n\t\t\t(0): All";
 	for (my $i=0; $i < $size; $i++) {
 		my $x = $i+1;
 		print "\n\t\t\t($x): ".$WEBS[$i][0];
@@ -78,7 +99,7 @@ sub AskPage {
 	my $pn = <STDIN>;
 	chop($pn);
 	
-	unless ($pn =~ m/[cC1-5]+/) {
+	unless ($pn =~ m/[cC0-$size]+/) {
 		$pn = AskPage();
 	}
 	
@@ -90,67 +111,77 @@ sub AskPage {
 #
 sub GetLinks {
 	my $response;
-	
-	my $separator 		= $WEBS[$se][2];
-	my $type_of_search 	= $WEBS[$se][3];
-	
-	$search_word = ReplaceSpaces($search_word, $separator);
+	my $separator = $WEBS[$se][2];
+	my $type_of_search = $WEBS[$se][3];
+
+	my $sw = ReplaceSpaces($search_word, $separator);
 	
 	switch ($type_of_search) {
-		case 1 { 
-			$response = $browser->post($SEARCH_URL, ['q' => $search_word, 'submit' => 'submit']); 
+		case 1 {
+			$response = $browser->post($SEARCH_URL, ['q' => $sw, 'submit' => 'submit']); 
 		}
-		case 2 {
-			$response = $browser->post($SEARCH_URL.$search_word);
-			$search_word = ReplaceSpaces($search_word, $HYPHEN);
+		case 2 {	
+			$response = $browser->post($SEARCH_URL.$sw);
 		}
 	}
 	
-	return GetDownloads( $response->content, undef );
+	return $response->content;
 }
 
 #
 # Get downloads for each page
 #
-sub GetDownloads {
+sub GetDownloadsURL {
 	my $web = shift;
 	my $round = shift;
-	my @clean_urls;
+	my @clean_urls;	
 
 	my $type_of_download = $WEBS[$se][4];
 
 	switch( $type_of_download ) {
 		case 1 {
-			@clean_urls = GetDownloads1($web);	
+			@clean_urls = GetDownloadsURL1($web);	
 		}
 		case 2 {
-			@clean_urls = GetDownloads2($web);
+			@clean_urls = GetDownloadsURL2($web);
 		}
 		case 3 {
-			@clean_urls = GetDownloads3($web, $round);
+			@clean_urls = GetDownloadsURL3($web, $round);
 		}
 	}
 	
 	@clean_urls = Distinct(@clean_urls);
 	
-	$downloads_size = @clean_urls;
+	return @clean_urls;
+}
+
+
+sub GetDownloadsTorrent {
+	my $round = shift;
+	my @urls = (@_);
+		
+	$downloads_size = @urls;
 	
 	print "\n|";
 	print Line();
-	
+		
 	unless($round) {
 		print "\n|\t$downloads_size ocurrences found with '$search_word':\n|";
 	}
-	
-	for (my $i=0; $i<$downloads_size; $i++) {
-		print "\n|\t(".($i+1)."): \t$clean_urls[$i]\n|";
+		
+	for (my $i=0; $i < $downloads_size; $i++) {
+		print "\n|\t(".($i+1)."): \t$urls[$i]\n|";
 	}
-	
+		
 	print Line();
 	print "\n";
-
+		
 	if ($downloads_size != 0) {
-		$selected_url = $BASE_URL.$clean_urls[(GetDownloadNumber()-1)];
+		if ($initial_se eq "0") {
+			$selected_url = $urls[(GetDownloadNumber()-1)];
+		} else {
+			$selected_url = $BASE_URL.$urls[(GetDownloadNumber()-1)];	
+		}
 		my $response = $browser->post($selected_url);
 		GetTorrent( $response->content );
 	}
@@ -159,13 +190,13 @@ sub GetDownloads {
 #
 # Get clean url downloads1
 #
-sub GetDownloads1 {
+sub GetDownloadsURL1 {
 	my $web = shift;
 	my @lines = split /\n/, $web;
 	my @downloads;
 	my @urls;
 	my @clean_urls;
-	
+
 	foreach my $line (@lines) {
 		$line = lc $line;
 		if ($line =~ m/\<a href=\"$BASE_URL\/descargar/ && $line =~ lc $search_word) {
@@ -179,7 +210,7 @@ sub GetDownloads1 {
 		
 	foreach (@urls) {
 		if ($_ =~ m/^$BASE_URL/) {
-			$_ =~ s/$BASE_URL//;
+			$_ =~ s/$BASE_URL// unless ($initial_se eq "0");
 			push @clean_urls, $_;
 		}
 	}
@@ -190,7 +221,7 @@ sub GetDownloads1 {
 #
 # Get clean url downloads2
 #
-sub GetDownloads2 {
+sub GetDownloadsURL2 {
 	my $web = shift;
 	my @lines = split /\n/, $web;
 	my @downloads;
@@ -213,7 +244,7 @@ sub GetDownloads2 {
 		
 	foreach (@urls) {
 		if ($_ =~ m/^$BASE_URL/) {
-			$_ =~ s/$BASE_URL//;
+			$_ =~ s/$BASE_URL// unless ($initial_se eq "0");
 			push @clean_urls, $_;
 		}
 	}
@@ -224,7 +255,7 @@ sub GetDownloads2 {
 #
 # Get clean url downloads3
 #
-sub GetDownloads3 {
+sub GetDownloadsURL3 {
 	my $web = shift;
 	my $round = shift;
 	my @lines = split /<a href='/, $web;
@@ -235,8 +266,7 @@ sub GetDownloads3 {
 	foreach my $line (@lines) {
 		$line = lc $line;
 		if ($line =~ lc $search_word) {
-			print "\nLinea: ".$line;
-			unless ($line =~ "musica" || $line =~ "DOCTYPE" || $line =~ "juego-descargar") {
+			unless ($line =~ "musica" || $line =~ "doctype" || $line =~ "juego-descargar") {
 				push @downloads, $line;
 			}
 		}
@@ -247,10 +277,13 @@ sub GetDownloads3 {
 		push @urls, $download;
 	}
 		
-	foreach (@urls) {
-		push @clean_urls, $_;
+	foreach my $url (@urls) {
+		if ($initial_se eq "0") {
+			$url = $BASE_URL.$url;
+		}
+		push @clean_urls, $url;
 	}
-	
+
 	return @clean_urls;
 }
 
@@ -289,8 +322,13 @@ sub GetTorrent {
 		mkdir $DEST_FOLDER;
 	}
 	
+	if ($initial_se eq "0") {
+		$BASE_URL = GetSelectedURLBase( $selected_url );
+		$se = GetElementPositionInArray("0", $BASE_URL, "first");
+	}
+	
 	my $type_of_torrent = $WEBS[$se][5];
-
+	
 	switch( $type_of_torrent ) {
 		case 1 {
 			$count = GetTorrent1(@lines);
@@ -306,8 +344,9 @@ sub GetTorrent {
 	if ($web_call) { $count = 1; } 
 	unless ($count) {
 		print "\n\n\tNo torrents found, it seems that there are several links:";
-		my $response = $browser->post( $selected_url );
-		GetDownloads( $response->content, 1 );
+		my $response = $browser->post( $selected_url );	
+		my @urls = GetDownloadsURL( $response->content, 1 );
+		GetDownloadsTorrent( 1, @urls );		
 	}
 }
 
@@ -317,7 +356,6 @@ sub GetTorrent {
 sub GetTorrent1 {
 	my @lines = (@_);
 	my $count = 0;
-	
 	foreach my $line (@lines) {
 		if ($line =~ m/window.location.href/) {
 			$line =~ s/^\s+window.location.href = \"//;
@@ -363,13 +401,14 @@ sub GetTorrent3 {
 		$line =~ s/.+\<a href=\'//;
 		if ($line =~ m/.torrent\'/) {
 			$line =~ s/\'.+//;
+			$selected_url = $BASE_URL."/".$line;
 			$count = DownloadFile($BASE_URL."/".$line, $BASE_URL."/".$line);
 		}
 		
 		unless( $web_call ) {
 			if ($line =~ m/contar&/) {
 				$line =~ s/\'.+//;
-				my $selected_url = $BASE_URL."/".$line;
+				$selected_url = $BASE_URL."/".$line;
 				my $response = $browser->post($selected_url);
 				$web_call = 1;
 				GetTorrent( $response->content );
@@ -408,9 +447,44 @@ sub ReplaceSpaces {
 	my $word = shift;
 	my $replace_character = shift;
 	$word =~ s/ /$replace_character/g;
-	$word =~ tr/"'/ /;
-	$word =~ tr/ //ds;
 	return $word;
+}
+
+#
+# Gets Element Position in Array
+#
+sub GetElementPositionInArray {
+	my $element = shift;
+	my $search = shift;
+	my $search_element = shift;
+	my $position = 0;
+	
+	for (my $i=0; $i < $size; $i++) {
+		if ($search_element eq "first") {
+			if ($WEBS[$i][$element] eq $search) {
+				$position = $i;
+			}
+		} elsif ($search_element eq "second") {
+			if ($WEBS[$element][$i] eq $search) {
+				$position = $i;
+			}
+		}
+	}
+
+	return $position;
+}
+
+
+#
+# Get URL BASE from $selected_url
+#
+sub GetSelectedURLBase {
+	my $url = $selected_url;
+	$url =~ s/http:\/\///;
+	$url =~ s/\/.+//;
+	$url = "http://".$url;
+	
+	return $url;
 }
 
 #
